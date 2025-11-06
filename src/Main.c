0895599199cbc3bead0842aefff40ec7bc897420
codeleaded <codeleaded@gmail.com>
2025-11-06 18:18:33 +0100

    update

--- src/Main.c
+++ src/Main.c
@@ -1,5 +1,14 @@
-//#include "C:/Wichtig/System/Static/Library/WindowEngine1.0.h"
+#if defined __linux__
 #include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#elif defined _WINE
+#include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#elif defined _WIN32
+#include "F:/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#elif defined(__APPLE__)
+#error "Apple not supported!"
+#else
+#error "Platform not supported!"
+#endif
 
 typedef struct vec3d{
 	float x;
